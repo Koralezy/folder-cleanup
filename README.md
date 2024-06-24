69a640549ea33d8480977abae758a6c8f7bbdae7
Koralezy <titoff21@icloud.com>
2024-06-23 19:14:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Folder Cleanup
+Folder Cleanup moves files that contain a certain keyword or phrase from one folder to another. You can specify a list of key words and/or phrases, the folder to scan through, and another folder to move the flagged files to!
+# How to Use
+There should be no requirements. Any version of Python should work, but it was coded in **Python 3.9.10**. Just run the file and follow the directions!
+
+![image](https://github.com/Koralezy/folder-cleanup/assets/100102260/ed31762b-c862-47c6-a27d-6a16f89c0cfc)
---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+
+print("Type your keywords/phrases separated by new lines and press ENTER twice when done.")
+keywds = []
+while True:
+    e = input()
+    if e == "":
+        break
+    else:
+        keywds.append(e)
+
+
+startdir = input("Type the origin folder path here: ")
+try:
+    startdir = startdir.replace('"', '')
+except:
+    pass
+
+enddir = input("Type the destination folder path here: ")
+try:
+    enddir = enddir.replace('"', '')
+except:
+    pass
+
+while True:
+    if enddir == startdir:
+        enddir = input("The origin cannot be the same as the destination. Please type a path to a DIFFERENT folder: ")
+    else:
+        break
+
+
+errorlist = {}
+for filename in os.listdir(startdir):
+    file = os.path.join(startdir, filename)
+
+    if os.path.isfile(file):
+        for word in keywds:
+            if word.lower() in file.lower():
+                newfile = os.path.join(enddir, filename)
+                try:
+                    os.rename(file, newfile)
+                    print("Moved", filename, " Keyword:", word)
+                except Exception as error:
+                    fullerr = f"{type(error).__name__} - {error}"
+                    print("An error occurred: ",fullerr)
+                    errorlist[filename] = fullerr
+
+
+print("Done!")
+if len(errorlist) > 0:
+    a = input(f"We found {len(errorlist)}, errors. Press ENTER to see them.")
+    if a != "" or a == "":
+        for x, y in errorlist.items():
+            print(f"File: {x} - Error: {y}")
